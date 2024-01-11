e11e888cb3bb37b3a1c16d136fe5cb4fd1e78d0f
Christopher Ndung'u <dev.kiratu@gmail.com>
2024-01-11 10:54:57 +0300

    add password validator

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -12,3 +12,10 @@ def hash_password(password: str) -> bytes:
     """
     hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    validate that the provided password matches the hashed password
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
