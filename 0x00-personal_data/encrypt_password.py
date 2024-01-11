c23b71dce866cc48c3a731f028b25fb57e770f1a
Christopher Ndung'u <dev.kiratu@gmail.com>
2024-01-11 10:43:45 +0300

    add a password hasher

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+encryption module using bcrypt
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> str:
+    """
+    encrypts a password using bcrypt and returns a salted, hashed password
+    which is a byte string.
+    """
+    hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    return hashed
