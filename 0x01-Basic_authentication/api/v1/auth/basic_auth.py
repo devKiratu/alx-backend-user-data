e3d2c9cefb2dd06548348228f27ce94f0623ff6a
Christopher Ndung'u <dev.kiratu@gmail.com>
2024-01-16 12:15:53 +0300

    validate all users matching search params

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -73,7 +73,7 @@ class BasicAuth(Auth):
         users = User.search({'email': user_email})
         if len(users) == 0:
             return None
-        user = users[0]
-        if user.is_valid_password(user_pwd):
-            return user
+        for user in users:
+            if user.is_valid_password(user_pwd):
+                return user
         return None
