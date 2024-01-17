774de35698b452b4b8352996c545c5c3d175e0a7
Christopher Ndung'u <dev.kiratu@gmail.com>
2024-01-17 12:44:52 +0300

    add task 5 solutions

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -35,12 +35,16 @@ def handle_auth():
         return
     excluded_paths = ['/api/v1/status/',
                       '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
+                      '/api/v1/forbidden/',
+                      '/api/v1/auth_session/login/']
     if auth.require_auth(request.path, excluded_paths):
         if auth.authorization_header(request) is None:
             abort(401)
         if auth.current_user(request) is None:
             abort(403)
+        if not auth.authorization_header(request) and\
+                not auth.session_cookie(request):
+            abort(401)
         else:
             request.current_user = auth.current_user(request)
 
