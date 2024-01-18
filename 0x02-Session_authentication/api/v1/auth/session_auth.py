b85d4119f4ad88c5cc9a943b1fca1ae205fdf1ba
Christopher Ndung'u <dev.kiratu@gmail.com>
2024-01-18 10:18:20 +0300

    implement identigying a user using a session ID

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,6 +3,7 @@
 Defines the Session Authentication module
 """
 from api.v1.auth.auth import Auth
+from models.user import User
 import uuid
 
 
@@ -33,3 +34,12 @@ class SessionAuth(Auth):
         if not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """
+        returns a User instance based on a cookie value
+        """
+        session_id = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_id)
+        user = User.get(user_id)
+        return user
