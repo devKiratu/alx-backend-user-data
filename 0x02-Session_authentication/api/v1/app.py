5d7636093f20e43b03be6555180f61b41c032e67
Christopher Ndung'u <dev.kiratu@gmail.com>
2024-01-18 09:38:28 +0300

    fix auth header and cookie validation

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -38,13 +38,11 @@ def handle_auth():
                       '/api/v1/forbidden/',
                       '/api/v1/auth_session/login/']
     if auth.require_auth(request.path, excluded_paths):
-        if auth.authorization_header(request) is None:
-            abort(401)
-        if auth.current_user(request) is None:
-            abort(403)
         if not auth.authorization_header(request) and\
                 not auth.session_cookie(request):
             abort(401)
+        if auth.current_user(request) is None:
+            abort(403)
         else:
             request.current_user = auth.current_user(request)
 
