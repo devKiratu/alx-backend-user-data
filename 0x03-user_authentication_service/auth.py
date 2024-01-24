fddc0c6998b54ce180dc2133df9db98a84b0883d
Christopher Ndung'u <dev.kiratu@gmail.com>
2024-01-24 12:06:29 +0300

    convert uuid to str

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -100,7 +100,7 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(email=email)
-            reset_token = uuid.uuid4()
+            reset_token = str(uuid.uuid4())
             self._db.update_user(user.id, reset_token=reset_token)
             return reset_token
         except Exception:
